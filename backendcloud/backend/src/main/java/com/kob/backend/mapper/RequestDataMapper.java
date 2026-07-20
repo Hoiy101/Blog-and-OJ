22c674991b4e4900e974f119b016e49236dcae9d
heyunchao <2985831679@qq.com>
2026-07-20 22:22:56 +0800

    添加mapper内容

--- backendcloud/backend/src/main/java/com/kob/backend/mapper/RequestDataMapper.java
+++ backendcloud/backend/src/main/java/com/kob/backend/mapper/RequestDataMapper.java
@@ -2,7 +2,9 @@ package com.kob.backend.mapper;
 
 import com.kob.backend.pojo.RequestData;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface RequestDataMapper {
 
     @Insert("INSERT INTO requestData (number, code, language, input, is_self_test) VALUES (#{number}, #{code}, #{language}, #{input}, #{isSelfTest})")
